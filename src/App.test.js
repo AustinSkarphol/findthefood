e0c82640eb0d1b09b2168ebc76974dc7bf842b09
austin skarphol <austin@austins-MacBook-Pro.local>
2022-08-24 11:44:45 -0700

    fixing test

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const linkElement = screen.getByText(/learn react/i);
+  render(<App />);
+  const linkElement = screen.getByText('Learn React')
   expect(linkElement).toBeInTheDocument();
 });
