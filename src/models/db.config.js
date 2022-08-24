7b1d224c7554d1479b3623cb39e7e5ac545c213b
austin skarphol <austin@austins-MacBook-Pro.local>
2022-08-24 14:05:27 -0700

    Azure Fix

--- src/models/db.config.js
+++ src/models/db.config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 module.exports = {
-    HOST: process.env.MYSQL,
+    HOST: 'bendfoodtrucks.mysql.database.azure.com',
     USER:  process.env.MYSQL_USER,
     PASSWORD: process.env.MYSQL_PW,
     DB: process.env.MYSQL_DB,
