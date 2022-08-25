1101ced663ed0e1160beae414409326b82ce9bc4
austin skarphol <austin@austins-MacBook-Pro.local>
2022-08-24 19:03:17 -0700

    Fixing Azure stuff

--- src/models/db.config.js
+++ src/models/db.config.js
@@ -1,9 +1,9 @@
 require("dotenv").config()
 module.exports = {
-    HOST: process.env.MYSQL,
-    USER:  process.env.MYSQL_USER,
-    PASSWORD: process.env.MYSQL_PW,
-    DB: process.env.MYSQL_DB,
+    HOST: process.env["MYSQL"],
+    USER:  process.env["MYSQL_USER"],
+    PASSWORD: process.env["MYSQL_PW"],
+    DB: process.env["MYSQL_DB"],
     dialect: "mysql",
     pool: {
       max: 5,
