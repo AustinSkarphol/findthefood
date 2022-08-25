e31bb474f686c253f7149cb629d3daa38a7a1c0a
austin skarphol <austin@austins-MacBook-Pro.local>
2022-08-24 18:37:33 -0700

    Fixing process envs

--- src/models/db.config.js
+++ src/models/db.config.js
@@ -1,6 +1,6 @@
-require("dotenv").config();
+require("dotenv").config()
 module.exports = {
-    HOST: process.env.AZURE_MYSQL_HOST,
+    HOST: process.env.MYSQL,
     USER:  process.env.MYSQL_USER,
     PASSWORD: process.env.MYSQL_PW,
     DB: process.env.MYSQL_DB,
