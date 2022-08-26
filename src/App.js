bb1b666f08efccb7237c7de90b3275844635a411
austin skarphol <austin@austins-MacBook-Pro.local>
2022-08-26 13:52:52 -0700

    creating truck registration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import './styles/styles.css'
 import Header from './components/register/header';
--- src/components/register/registration.js
+++ src/components/register/registration.js
@@ -1,4 +1,4 @@
-import React, {useState,setState} from 'react';
+import React, {useState} from 'react';
 import '../../styles/styles.css'
 import truckRegister from'../../services/truck_registration' 
 
