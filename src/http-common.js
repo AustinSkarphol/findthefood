e4956682cad630104adb3389c9e424a30e12b53b
austin skarphol <austin@austins-MacBook-Pro.local>
2022-08-26 14:14:42 -0700

    fixing pathing

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+require("dotenv").config()
+
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
     "Content-type": "application/json"
   }
